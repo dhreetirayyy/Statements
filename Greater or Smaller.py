08f4ce8b9be349a9b33d0ffc7af3525a5957e454
dhreetirayyy <dkumar.deo@gmail.com>
2025-03-17 12:23:06 -0700

    Greater or Less Than

---
+++ Greater or Smaller.py
@@ -0,0 +1,7 @@
+i = int(input("Enter a Number:"))
+if (i < 20):
+    print("i is smaller than 20")
+    print("Im in If Block")
+else:
+    print("i is greater than 20")
+    print("Im in ELSE block")
